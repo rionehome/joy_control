e5246b6e4edf00fcafa4fadc3592f16f4018a762
Hara-kun <jhara@asl.cs.ritsumei.ac.jp>
2023-04-18 13:38:32 +0900

    update: don't use direction key

--- src/joy_control.py
+++ src/joy_control.py
@@ -5,26 +5,19 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
 # adjust these values
-LINEAR_SPEED = 0.25
+LINEAR_SPEED = 0.15
 ANGULAR_SPEED = 1.25
 
 class JoyControl():
     def __init__(self):
         rospy.init_node("joy_control")
-        self.velocity_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size = 10)
+        self.velocity_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size = 1)
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_callback)
     
     def joy_callback(self, msg):
         # joystick values
         x = msg.axes[1] * LINEAR_SPEED
         z = msg.axes[0] * ANGULAR_SPEED
-        
-        # override values when direction key is used
-        if msg.axes[7] != 0:
-            x = msg.axes[7] * LINEAR_SPEED
-        
-        if msg.axes[6] != 0:
-            z = msg.axes[6] * ANGULAR_SPEED
         
         if x == 0 and z == 0:
             return
